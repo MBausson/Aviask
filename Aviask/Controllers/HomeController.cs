17eff496d4f80ba3d68e11374e108508a9ab2bdf
MBausson <maximilienbausson@icloud.com>
2023-07-06 22:00:56 +0200

    Files cleanup

--- Aviask/Controllers/HomeController.cs
+++ Aviask/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using Aviask.Models;
 using Aviask.Models.ResponseModels;
 using Aviask.Repositories;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -34,59 +33,6 @@ namespace Aviask.Controllers
             }
 
             return View();
-        }
-
-        [Authorize]
-        public async Task<IActionResult> UserStatistics()
-        {
-            var id = _userManager.GetUserId(User);
-            
-            var userRecordsQuery = _answerRecordsRepository.GetAll();
-            userRecordsQuery = userRecordsQuery.Where(r => r.UserId == id);
-
-            //  Counts how much questions were answered since last 7 days
-            var now = DateTime.Now;
-            var lastWeekRecords = (await userRecordsQuery.ToListAsync()).Where(r => (now - r.Date).TotalDays < 7).Count();
-
-            //  Calculates lifetime answer correctness
-            int correctLifetime = await userRecordsQuery.Where(r => r.CorrectAnswer).CountAsync();
-            int failLifetime = await userRecordsQuery.Where(r => !r.CorrectAnswer).CountAsync();
-            float ratio;
-
-            if (failLifetime + correctLifetime != 0) 
-            {
-                ratio = (float)correctLifetime / (failLifetime + correctLifetime);
-            } else
-            {
-                ratio = 0;
-            }
-
-            //  Computes the 3 most answered subcategories
-            var mostAnsweredThemes = userRecordsQuery
-                .AsEnumerable()
-                .GroupBy(r => r.Question.SubCategory)
-                .Select(g => new StatisticsCategoryResponse(g.Key, g.Key.ToString(), g.Count()))
-                .OrderByDescending(g => g.AnswerCount)
-                .Take(3)
-                .ToList();
-
-            //  Computes the most correctly answered category
-            var mostCorrectlyTheme = userRecordsQuery
-                .AsEnumerable()
-                .Where(r => r.CorrectAnswer)
-                .GroupBy(r => r.Question.SubCategory)
-                .Select(g => new StatisticsCategoryResponse(g.Key, g.Key.ToString(), g.Count()))
-                .OrderByDescending(g => g.AnswerCount)
-                .FirstOrDefault();
-
-            return Ok(new UserStatisticsResponse(
-                lastWeekRecords,
-                correctLifetime,
-                failLifetime,
-                ratio,
-                mostCorrectlyTheme!,
-                mostAnsweredThemes
-            ));
         }
 
         public IActionResult Privacy()
---
+++ Aviask/wwwroot/js/tailwind.config.js
@@ -0,0 +1,13 @@
+/*global tailwind*/
+
+tailwind.config = {
+    theme: {
+        screens: {
+            sm: "576px",
+            md: "768px",
+            lg: "992px",
+            xl: "1200px",
+            xxl: "1400px",
+        },
+    },
+};
